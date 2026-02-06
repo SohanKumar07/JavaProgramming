3bfe2047aa86ed45d43caac15fc0cc5c77a0581b
SohanKumar07 <infosohan6@gmail.com>
2026-02-06 17:08:31 +0530

    first commit

---
+++ src/Chapter_01/ss_01_first.java
@@ -0,0 +1,15 @@
+package Chapter_01;
+
+public class ss_01_first {
+    public static void main(String[] args) {
+        System.out.println("Sohan Singh");
+
+        // single line comments using --> //(forward slash)
+
+        /*
+        multiline comments using shortcut --> ctr + shift + /
+
+        */
+
+    }
+}
