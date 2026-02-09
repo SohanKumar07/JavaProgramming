5b1c9cde5424e5a92e6d174e4f7ac696b038d41c
SohanKumar07 <infosohan6@gmail.com>
2026-02-09 10:53:47 +0530

    8-2 updated

---
+++ src/Variables_02/ss_01_variables.java
@@ -0,0 +1,13 @@
+package Variables_02;
+
+public class ss_01_variables {
+    public static void main(String[] args) {
+
+        //dataType variableName = value;
+
+        int age = 21;
+        System.out.println(age);
+        System.out.println("My age is: " + age);
+
+    }
+}
---
+++ src/Variables_02/ss_02_var_example.java
@@ -0,0 +1,22 @@
+package Variables_02;
+
+public class ss_02_var_example {
+    public static void main(String[] args) {
+        
+        int age = 25;
+        double price = 19.99;
+        String name = "Sohan Singh";
+        boolean isActive = true;
+        char grade = 'A';
+        float temperature = 98.6f;
+        long population = 7800000000L;
+
+        System.out.println("Age is: "+ age);
+        System.out.println("The price is: " + price);
+        System.out.println("My name is: " + name);
+        System.out.println("Grade is: " + grade);
+        System.out.println("Active is: " + isActive);
+        System.out.println("Today temperature is: " + temperature);
+        System.out.println("Our country population is: " + population);
+    }
+}
