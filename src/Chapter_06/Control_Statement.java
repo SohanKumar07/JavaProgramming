04319cf0d55eefd95dcc13126fa3da02b9c51463
SohanKumar07 <infosohan6@gmail.com>
2026-02-24 07:19:55 +0530

    day7 updated

---
+++ src/Chapter_06/Control_Statement.java
@@ -0,0 +1,10 @@
+package Chapter_06;
+
+public class Control_Statement {
+    public static void main(String[] args) {
+
+        for(int i = 1; i <= 5; i++){
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/Chapter_06/whileLoop.java
@@ -0,0 +1,7 @@
+package Chapter_06;
+
+public class whileLoop {
+    public static void main(String[] args) {
+
+    }
+}
